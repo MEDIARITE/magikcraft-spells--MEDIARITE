7b3650226b8ee95a2cb38de53128873a616a9464
MEDIARITE <run.star.run@hotmail.com>
2017-12-01 11:09:15 +0000

    Update Admin.ts

---
+++ Admin.ts
@@ -0,0 +1,29 @@
+function Admin(name) {
+
+      
+      
+      const sender = magik.getSender();
+
+      
+      
+      const plugin = magik.getPlugin();
+
+      
+      
+      const server = plugin.getServer();
+
+      
+      
+      const consoleSender = server.getConsoleSender();
+
+      
+      
+      server.dispatchCommand(consoleSender,`pex  user ${name} group set admin`);
+
+      
+      
+      magik.dixit(`Granted admin to ${name}`);
+
+      
+      
+}
