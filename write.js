9f8fb1972874599d26c5c393d5b7f2ae7963ea53
MEDIARITE <run.star.run@hotmail.com>
2017-12-17 05:05:39 +0000

    Update write.js

--- write.js
+++ write.js
@@ -7,19 +7,19 @@ function write() {
     b:[192,192,192,192,252,194,194,252],
     c:[255,192,192,192,192,192,192,255],
     d:[254,195,195,195,195,195,195,225],
-    e:[252,128,128,252,]
-    f:[]
-    g:[]
-    h:[]
-    i:[]
+    e:[252,128,128,252,],
+    f:[],
+    g:[],
+    h:[],
+    i:[],
     }
 
-function binary(letter){
-    return letter.map(line=>
-    line.toString(2));
-}
+const binary=letter=>
+    letter.map(line=>
+    line.toString(2).padStart(8,"0"))
+
 
 const a = binary(font.a);
-a.forEach(line=>magik.dixit(line.padStart("0",8)));
+a.forEach(line=>magik.dixit(line))
 
 }
