cba66f3a417331a6decfd35afee8898d0d0af0e3
MEDIARITE <run.star.run@hotmail.com>
2017-12-19 03:12:50 +0000

    Update write.js

--- write.js
+++ write.js
@@ -1,28 +1,40 @@
 const magik = magikcraft.io;
 
-function write(char="a") {
-	
-    const font = {
-    a:[0,24,36,102,126,126,102,102],
-    b:[192,192,192,192,252,194,194,252],
-    c:[255,192,192,192,192,192,192,255],
-    d:[254,195,195,195,195,195,195,225],
-    e:[252,128,128,252,128,128,128,252],
-    f:[0,62,32,62,32,32,32,32],
-    g:[255,255,192,192,199,195,255,255],
-    h:[102,102,102,126,102,102,102,102],
-    i:[255,126,24,24,24,24,126,255],
-}
 
-function _write(char = 'a') {
 
-    const STONE = magik.type('Material').STONE
+const fontData = {
+
+    a: [24 ,60, 36, 102, 126, 126, 102, 102],
+
+    b: [192, 192, 192, 192, 255, 194, 194, 252],
+
+    c: [255, 192, 192, 192, 192, 192, 192, 255],
+
+    d: [254, 195, 195, 195, 195, 195, 195, 254],
+
+    e: [252, 128, 128, 252, 128, 128, 128, 252],
+
+    f: [0, 62, 32, 62, 32, 32, 32, 32],
+
+    g: [255, 255, 192, 192, 199, 195, 255, 255],
+
+    h: [102, 102, 102, 126, 102, 102, 102, 102],
+
+    i: [255, 126, 24, 24, 24, 24, 126, 255]
+
+} 
+
+
+
+function write(letters = 'abc') {
+
+    const BLOCK = magik.type('Material').DIAMOND_BLOCK
 
 
 
     const number2binary = num => num.toString(2)
 
-    const addLeadingZeros = line => line.padStart(8, "0")
+    const addLeadingZeros = line => line.padStart(8, '0')
 
     const lineBlockArray = line => Array.from(addLeadingZeros(number2binary(line)))
 
@@ -42,14 +54,13 @@ function _write(char = 'a') {
 
 
 
-    const letterMap = binaryMap(font[char]);
+    const chars = Array.from(letters)
 
+    chars.forEach((char, letternum) => 
 
+        binaryMap(fontData[char]).forEach((line, linenum) => 
 
-    letterMap.forEach((line, linenum) => 
+        line.forEach((char, charnum) => (char == '1' &&
 
-        line.forEach((char, charnum) => (char == "1" &&
-
-            getBlock(x + charnum, y - linenum, z).setType(DIAMOND_BLOCK))));
-
+            getBlock(x + charnum + (letternum * 10), y - linenum, z).setType(BLOCK)))))
 }
