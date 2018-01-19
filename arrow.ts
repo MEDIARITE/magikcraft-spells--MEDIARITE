174305fdce995ca5120d3641152aaa02aeb8c2b4
MEDIARITE <run.star.run@hotmail.com>
2018-01-19 02:09:15 +0000

    Update arrow.ts

---
+++ arrow.ts
@@ -0,0 +1,23 @@
+const magik = magikcraft.io;
+
+
+
+function arrow() {
+
+
+
+    function shoot() {
+
+        const Arrow = Java.type("org.bukkit.entity.Arrow");
+
+        const p = magik.getSender();
+
+        p.launchProjectile(Arrow.class);
+
+    }
+
+
+
+    magik.doNTimes(shoot, 100, 50);
+
+}
